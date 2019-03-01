a18194b275b8ac416908162b75d52003d872adbf
tjohns96 <46728195+tjohns96@users.noreply.github.com>
2019-03-01 01:30:31 -0500

    Now has comment

--- Booling/src/booling/AnagramChecker.java
+++ Booling/src/booling/AnagramChecker.java
@@ -11,9 +11,14 @@ public class AnagramChecker {
       for (int i = 0; i < aList.length; i++) {
         if (aList[i] == bList[i]) {
           value = true;
+          // I think this continue here is redundant, but it makes it so that the loop starts a new
+          // iteration as soon as it hits the continue statement
           continue;
         } else {
           value = false;
+          // since I want the loop to stop as soon as I find a pair of chars that don't match,
+          // I put a break statement here so that the loop ends and a false value is immediately
+          // returned
           break;
         }
       }
