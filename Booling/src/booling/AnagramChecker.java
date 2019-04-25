0dbf33f92cb5ae86f721a4a9db12f811416df323
tjohns96 <46728195+tjohns96@users.noreply.github.com>
2019-04-24 22:31:54 -0400

    Updating with JavaDoc comments

--- Booling/src/booling/AnagramChecker.java
+++ Booling/src/booling/AnagramChecker.java
@@ -1,15 +1,30 @@
 package booling;
 
+import java.util.Locale;
+
+/**
+ * This is a class that checks if two words are anagrams.
+ * 
+ * @author Trevor Johnson
+ *
+ */
 public class AnagramChecker {
+  /**
+   * A method to check if two words are anagrams.
+   * 
+   * @param a The first string that you check to see if it is an anagram with b.
+   * @param b The other string that you want to check against a.
+   * @return Returns true if the words are anagrams, false if not.
+   */
   boolean isAnagram(String a, String b) {
-    char[] aList = a.toLowerCase().toCharArray();
-    char[] bList = b.toLowerCase().toCharArray();
-    java.util.Arrays.sort(aList);
-    java.util.Arrays.sort(bList);
+    char[] a1List = a.toLowerCase(Locale.ENGLISH).toCharArray();
+    char[] b1List = b.toLowerCase(Locale.ENGLISH).toCharArray();
+    java.util.Arrays.sort(a1List);
+    java.util.Arrays.sort(b1List);
     boolean value = false;
-    if (aList.length == bList.length) {
-      for (int i = 0; i < aList.length; i++) {
-        if (aList[i] == bList[i]) {
+    if (a1List.length == b1List.length) {
+      for (int i = 0; i < a1List.length; i++) {
+        if (a1List[i] == b1List[i]) {
           value = true;
           // I think this continue here is redundant, but it makes it so that the loop starts a new
           // iteration as soon as it hits the continue statement
