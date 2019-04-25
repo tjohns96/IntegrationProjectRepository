06ba26958f0ff98ccf7e42aa213fd739b232137f
tjohns96 <46728195+tjohns96@users.noreply.github.com>
2019-04-24 22:33:06 -0400

    A lot of changes
    I added constructors, javadoc comments, and default statements to the switches

--- Booling/src/booling/Card.java
+++ Booling/src/booling/Card.java
@@ -2,24 +2,44 @@ package booling;
 
 import java.util.Random;
 
+/**
+ * A class for a playing card.
+ * 
+ * @author Trevor Johnson
+ * 
+ */
 public class Card {
   private int cardNum;
   private String suite;
   private String numName;
-  
-  public Card(){
+
+  /**
+   * This is a parameterless constructor, it will make a 2 of spades for you.
+   */
+  public Card() {
     cardNum = 2;
     suite = "Spades";
     numName = "2";
   }
-public Card(String n, String s) {
+
+  /**
+   * This is a constructor that you can use to make a card.
+   * 
+   * @param n This parameter is what you use to set the card value, can be any number or Jack
+   *        through Ace. You can break a card by putting in a random string or int that is not from
+   *        2-10.
+   * @param s This is how you set the suite, again the card won't be right if you enter a suite that
+   *        doesn't exist.
+   * @see If you enter a string that is not Jack, Ace, Queen, or King, you get a "scuffed" card.
+   *      Avoid that
+   */
+  public Card(String n, String s) {
     numName = n;
     suite = s;
-    try{
+    try {
       cardNum = Integer.parseInt(n);
-    }
-    catch(NumberFormatException ex) {
-      switch(n) {
+    } catch (NumberFormatException ex) {
+      switch (n) {
         case "Jack":
           cardNum = 11;
           break;
@@ -38,6 +58,10 @@ public Card(String n, String s) {
       }
     }
   }
+
+  /**
+   * Will generate a random card that could be found in a standard deck.
+   */
   public void draw() {
     Random cardGen = new Random();
     int suiteVal = cardGen.nextInt(4);
@@ -111,19 +135,105 @@ public Card(String n, String s) {
         cardNum = cardVal;
         numName = "Ace";
         break;
+      default:
+        System.out.println("it's impossible to get here baka");
     }
   }
 
-  public void setSuite(String s) {
-    suite = s;
+  /**
+   * Overloading the draw method to draw a card with a specified card value and random suite.
+   * 
+   * @param n The integer value that you want to give your card, values 11-14 will be assigned Jack
+   *        through Ace
+   */
+  public void draw(int n) {
+    Random cardGen = new Random();
+    int suiteVal = cardGen.nextInt(4);
+    switch (suiteVal) {
+      case 0:
+        suite = "Hearts";
+        break;
+      case 1:
+        suite = "Diamonds";
+        break;
+      case 2:
+        suite = "Spades";
+        break;
+      case 3:
+        suite = "Clubs";
+        break;
+      default:
+        System.out.println("oof");
+    }
+    int cardVal = n;
+    switch (cardVal) {
+      case 2:
+        cardNum = cardVal;
+        numName = "2";
+        break;
+      case 3:
+        cardNum = cardVal;
+        numName = "3";
+        break;
+      case 4:
+        cardNum = cardVal;
+        numName = "4";
+        break;
+      case 5:
+        cardNum = cardVal;
+        numName = "5";
+        break;
+      case 6:
+        cardNum = cardVal;
+        numName = "6";
+        break;
+      case 7:
+        cardNum = cardVal;
+        numName = "7";
+        break;
+      case 8:
+        cardNum = cardVal;
+        numName = "8";
+        break;
+      case 9:
+        cardNum = cardVal;
+        numName = "9";
+        break;
+      case 10:
+        cardNum = cardVal;
+        numName = "10";
+        break;
+      case 11:
+        cardNum = cardVal;
+        numName = "Jack";
+        break;
+      case 12:
+        cardNum = cardVal;
+        numName = "Queen";
+        break;
+      case 13:
+        cardNum = cardVal;
+        numName = "King";
+        break;
+      case 14:
+        cardNum = cardVal;
+        numName = "Ace";
+        break;
+      default:
+        System.out.println("This card is now broken.");
+    }
   }
 
-  public void setNumName(String n) {
-    numName = n;
+  public void setSuite(String suite) {
+    this.suite = suite;
   }
 
-  public void setCardNum(int n) {
-    cardNum = n;
+  public void setNumName(String numName) {
+    this.numName = numName;
+  }
+
+  public void setCardNum(int cardNum) {
+    this.cardNum = cardNum;
   }
 
   public String getSuite() {
